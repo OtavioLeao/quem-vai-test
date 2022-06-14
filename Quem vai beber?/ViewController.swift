bf8c606dcbaa435eabf49bcf67151b95b04c57ee
Otávio Leão <otavio.ferreira@mercadolivre.com>
2022-06-14 19:25:38 -0300

    first commit

--- Quem vai beber?/ViewController.swift
+++ Quem vai beber?/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func playButton(_ sender: Any) {
+        
+        print("botao play" + textField.text! )
+    }
+    @IBAction func resetPlayers(_ sender: Any) {
+        print("botao reset")
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
